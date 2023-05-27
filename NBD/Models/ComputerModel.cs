e0e1ed3a3d2f26b5e98a95e2c5a85adb171284f2
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2023-05-27 19:36:57 +0200

    Add project files.

---
+++ NBD/Models/ComputerModel.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Buffers.Text;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NBD.Models;
+
+public class ComputerModel
+{
+
+	[BsonRepresentation(BsonType.ObjectId)]
+	public string? Id { get; set; }
+
+	[Display(Name = "Computer Name")]
+	public string? Name { get; set; }
+	[Display(Name = "Creation Year")]
+	public int? Year { get; set; }
+	public string? ImageId { get; set; }
+
+	public bool HasImage() => !string.IsNullOrWhiteSpace(ImageId);
+}
